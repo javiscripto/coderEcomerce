8e3a51b8aa9e529477ecb656fc98900e8e7b1409
javier.mecker94 <javier.mecker94@gmail.com>
2024-05-24 15:12:27 +0000

    save changes from idx

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useCallback} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Welcome } from './welcome';
+import { Welcome } from './screens/welcome';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
 
-
-
-
-
-
-
-
+SplashScreen.preventAutoHideAsync();
 
 
 export default function App() {
+  const [fontLoaded,fontError]=useFonts({
+    'Roboto-Black':require('./assets/fonts/Roboto-Black.ttf'),
+    'Roboto-Regular':require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold':require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Italic':require('./assets/fonts/Roboto-Italic.ttf'),
+    'Roboto-Medium':require('./assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Thin':require('./assets/fonts/Roboto-Thin.ttf'),
+    'Roboto-Light':require('./assets/fonts/Roboto-Light.ttf'),
+    
+  });
+
+
+  const onLayoutRootView =useCallback(async () => {
+    if (fontLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontLoaded, fontError]);
+
+
+  
   return (
     
     <SafeAreaProvider style={styles.container}>
-      <Welcome/>
-      
+      {/* <Welcome/> */}
+      <Text style={styles.title}>Hello</Text>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
@@ -30,4 +47,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontFamily:"Roboto-Black",
+    fontSize:64,
+    fontWeight:"bold",
+    color:"black"
+  }
 });
---
+++ assets/colorsTheme.js
@@ -0,0 +1,11 @@
+export const theme={
+    primary:'#000000',
+    black:'#000',
+    green:'#606c38',
+    dark_green:'#283618',
+    ligth_cream:'#f0f1e4',
+    brick:'#d62828',
+    terracota:'#e76f51',
+    cream:'#f0f1e4',
+
+}
